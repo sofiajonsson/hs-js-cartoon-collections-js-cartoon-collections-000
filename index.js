f910b77c872c269dd6e80d508c5b8a71017bac89
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-01-08 21:30:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-function dwarfRollCall(dwarves) {
+var names = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+function dwarfRollCall(dwarves) { 
+  dwarves.push(names);
+  return `1. ${names[1]} 2. ${names[3]} 3. ${names[2]}`;
 }
 
+var planets = ["earth", "wind", "fire", "water", "heart"]
 function summonCaptainPlanet(planeteerCalls){
+  planeteerCalls.push(planets.toUpperCase);
 }
 
 function longPlaneteerCalls(words) {
